9d7cccecb24e517605e6407beab5fef743fc0904
Petter Sjulstad <petter.sjulstad@gmail.com>
2021-02-24 12:26:55 +0100

    removed the else condition

--- lib/groupdate/series_builder.rb
+++ lib/groupdate/series_builder.rb
@@ -44,12 +44,12 @@ module Groupdate
             else
               k = key
             end
-            verified_data[k] = v
-          elsif key != round_time(key)
+            # verified_data[k] = v
+          # elsif key != round_time(key)
             # only need to show what database returned since it will cast in Ruby time zone
             # raise Groupdate::Error, "Database and Ruby have inconsistent time zone info. Database returned #{key}"
           end
-        end
+        # end
       end
 
       unless entire_series?(series_default)
